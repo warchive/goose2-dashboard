1449df8f0f21e20a91687f3efff5006941fcc503
Tyler Zhang <TZTylerzhang@gmail.com>
2017-08-22 18:12:22 -0400

    fixed scaling issues, successfully test graphs on tab view

--- src/section/visuals/ECGraphs.jsx
+++ src/section/visuals/ECGraphs.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
-import { TabView } from '../../components/Tabs'
+import { TabView, Tab } from '../../components/Tabs'
 import { connect } from 'react-redux'
 import LiveChart from '../../components/LiveChartMulti'
 
 const ECGraph = ({ style, photo, temp }) => {
   return (
-    <TabView tabNames={['1', '2', '3']} style={style}>
-      <LiveChart
-        height={130}
-        title='Pressure'
-        min={0}
-        max={100}
-        columnNames={['1', '2']}
-        data={photo}
-      />
-      <LiveChart
-        height={130}
-        title='temperature'
-        min={0}
-        max={100}
-        columnNames={['1', '2']}
-        data={temp}
-      />
+    <TabView tabNames={['T', 'P']} style={style}>
+      <Tab>
+        <LiveChart
+          height={130}
+          title='temperature'
+          min={0}
+          max={100}
+          columnNames={['1', '2']}
+          data={temp}
+        />
+      </Tab>
+      <Tab>
+        <LiveChart
+          height={130}
+          title='Photoelectric'
+          min={0}
+          max={100}
+          columnNames={['1', '2']}
+          data={photo}
+        />
+      </Tab>
     </TabView >
   )
 }
